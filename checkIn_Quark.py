f31a4d26a983a9f45391862fbeada5642368bf4c
Luciusbjx <2736326274@qq.com>
2026-06-08 10:07:46 +0300

    Update checkIn_Quark.py

--- checkIn_Quark.py
+++ checkIn_Quark.py
@@ -11,11 +11,11 @@ Description:
 抓包流程：
     【手机端】
     ①打开抓包，手机端访问抽奖页
-    ②找到url为 https://drive-m.quark.cn/1/clouddrive/act/growth/reward 的请求信息
+    ②找到url为 https://drive-m.quark.cn/1/clouddrive/capacity/growth/info 的请求信息
     ③复制整段url，该链接后面必须要有参数: kps sign vcode，粘贴到环境变量
     环境变量名为 COOKIE_QUARK 多账户用 回车 或 && 分开
     user字段是用户名 (可是随意填写，多账户方便区分)
-    例如: user=张三; url=https://drive-m.quark.cn/1/clouddrive/act/growth/reward?xxxxxx=xxxxxx&kps=abcdefg&sign=hijklmn&vcode=111111111;
+    例如: user=张三; url=https://drive-m.quark.cn/1/clouddrive/capacity/growth/info?xxxxxx=xxxxxx&kps=abcdefg&sign=hijklmn&vcode=111111111;
     旧版环境变量格式也兼容，例如: user=张三; kps=abcdefg; sign=hijklmn; vcode=111111111;
 '''
 import os
